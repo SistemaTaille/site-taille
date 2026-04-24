656cb38cdefdbd356154b2f00b3c29dd9249c77f
Pedro Reuter <pedroreuter.taille@gmail.com>
2026-04-24 14:38:43 -0300

    fix: base dinâmico para github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/site-taille/", // 👈 ISSO AQUI
+  base: mode === "production" ? "/site-taille/" : "/",
 
   server: {
     host: "::",
